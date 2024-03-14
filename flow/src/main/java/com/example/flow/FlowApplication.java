3f7a22f366ecaba353d96b6801393713313633f7
gt.park <smileboy0014@gmail.com>
2024-03-14 17:18:20 +0900

    [대기열시스템 #3 대기열 스케줄러 개발] - 1. UserQueueService 에서 스케줄러 method 개발 - 2. application.yml 에서 flag 값으로 스케줄러 작동 여부 조절

--- flow/src/main/java/com/example/flow/FlowApplication.java
+++ flow/src/main/java/com/example/flow/FlowApplication.java
@@ -2,7 +2,9 @@ package com.example.flow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling //스케줄러를 사용하기 위해서
 @SpringBootApplication
 public class FlowApplication {
 
--- flow/src/main/java/com/example/flow/service/UserQueueService.java
+++ flow/src/main/java/com/example/flow/service/UserQueueService.java
@@ -2,9 +2,13 @@ package com.example.flow.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
@@ -18,7 +22,11 @@ public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
 
     //대기열 등록 API
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -67,5 +75,27 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 3000) // 초기 앱 기동 후 initialDelay 초 뒤부터 시작, fixedDelay 초마다 메서드 호출
+    private void scheduleAllowUser() {
+        if (!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
+
+        log.info("called scheduling...");
+
+        long maxAllowUserCount = 300L; //몇명씩 진행열로 넣을 지
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100) //한번에 몇개의 종류의 대기열 셋을 가져올 지
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(allowed, queue)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT1(), tuple.getT2())))
+                .subscribe();
+
     }
 }
